67062b59e8f472f935767656f8c78dc0425d62cd
Kathy <catherine.omondi2@gmail.com>
2018-12-14 23:31:55 +0300

    [Feature #162673123] Create a new user account

---
+++ app/api/v1/models/user_models.py
@@ -0,0 +1,37 @@
+from flask import jsonify
+from ..utils.validators import Verify
+user_accounts = []
+
+
+class Accounts(Verify):
+    '''A class to represent the user model'''
+
+
+    def signup(self, email_address, username, password, timestamp):
+        '''method to add a user to user_accounts list'''
+        new_member = dict(
+            email_address = "email_address",
+            username = "username",
+            password = "password",
+            timestamp = "timestamp"
+        )
+        
+        member_info = [email_address, username, password]
+    
+        present = self.list_iterator(member_info)
+        if present is False:
+            return jsonify({"message": "Please fill out all fields"})
+        if self.is_signup_payload is False:
+            return jsonify({"message": "Payload is invalid"})
+        if self.is_valid_password(password) is True:
+            return jsonify({"message": "Password has to be between 6 and 12 characters long"})
+        if self.is_valid_email(email_address) is True:
+            return jsonify({"message": "Please enter a valid email address"})
+        else:
+            user_accounts.append(new_member)
+            return {
+                "message": "User with username {} added successfully".format(username),
+                "result": "Welcome to StackOverflow-Lite",
+                "Stackoverflow member since": timestamp
+                }
+        
---
+++ app/api/v1/views/user_views.py
@@ -0,0 +1,35 @@
+from flask import Flask, json, jsonify, request, make_response, Blueprint
+
+import datetime
+
+from ..models import user_models
+
+from ..utils.validators import Verify
+
+auth = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
+
+user_object = user_models.Accounts()
+
+@auth.route('/signup', methods = ['POST'])
+def register_user():
+    '''endpoint to create an account'''
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "Data set cannot be empty"}), 400
+    email_address = data.get('email_address').strip()
+    username = data.get('username').strip()
+    password = data.get('password').strip()
+    timestamp = datetime.datetime.now()
+
+    
+    result = jsonify(user_object.signup(email_address, username, password, timestamp))
+    result.status_code = 201
+    return result
+
+
+
+    
+
+
+    
+
